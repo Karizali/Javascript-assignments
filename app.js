6db67dd9b8257812aa503680d18e949bdd64b40f
Karizali <96954751+Karizali@users.noreply.github.com>
2022-10-31 17:27:48 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,76 @@
+// Chapter 1 start
+alert("My Name is Syed Kariz Ali");
+// --------------ends------------------
+
+// Chapter 2 Start
+var names = "This is My First JavaScript Program";
+alert(names)
+alert("Let me show you my javascript practices.");
+// --------------ends------------------
+
+// Chapter 3 start
+var name_1 = 30;
+var name_2 = 40;
+total =name_1 + name_2
+alert(total)
+var name_1 = 30;
+var name_2 = 40;
+total =name_2 - name_1
+alert(total)
+var name_1 = 30;
+var name_2 = 40;
+total =name_1/name_2
+alert(total)
+var name_1 = 30;
+var name_2 = 40;
+total =name_1*name_2
+alert(total)
+// --------------ends------------------
+
+// Chapter 4 start
+                    // legal Variable:
+alert("Legal variables: name, Name, name1, _name, Name, nAme")
+// name 
+// Name 
+// name1
+// _name 
+// Name 
+// nAme 
+alert("Illegal variables: 1name, @name, name@, n@me")
+                    // illegal Variable:
+// 1name
+// @name
+// name@
+// n@me
+// --------------ends------------------
+
+// Chapter 5 start
+var xyz = 90;
+total = xyz + 20
+alert(total)
+// --------------ends------------------
+
+// Chapter 6 start
+var num1 = 71;
+num1++;
+alert(num1)
+console.log(num1)
+// --------------ends------------------
+
+// Chapter 7 start
+var amount = 5 - 3 / 9;
+alert(amount)
+console.log(amount)
+var amount = (5 * 7)/4 + 8;
+alert(amount)
+console.log(amount)
+// --------------ends------------------
+
+// Chapter 8 start
+var username = "For your Input"
+alert("Thank you, " + username + "!");
+// --------------ends------------------
+
+
+
+
